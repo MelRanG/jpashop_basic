c43ee3c3193483b58ed370336dd7e9d18caa789e
MelRanG <tpdud285@gmail.com>
2024-04-08 20:02:33 +0900

    관계매핑 추가

---
+++ src/main/java/jpabook/jpashop/domain/Category.java
@@ -0,0 +1,32 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+public class Category {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+    //자신의 상위 카테고리. 셀프 매핑도 가능
+    @ManyToOne
+    @JoinColumn(name = "PARENT_ID")
+    private Category parent;
+
+    //하위 카테고리
+    @OneToMany(mappedBy = "parent")
+    private List<Category> child = new ArrayList<>();
+    @ManyToMany
+    @JoinTable(name = "CATEGORY_ITEM",
+            joinColumns = @JoinColumn(name = "CATEGORY_ID"),
+            inverseJoinColumns = @JoinColumn(name = "ITEM_ID")
+    )
+    private List<Item> items = new ArrayList<>();
+}
